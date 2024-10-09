40666632991545d76243d8eb0469811399f7b515
CamTruong247 <CamTruong247@gmail.com>
2024-10-09 22:54:17 +0700

    Update - 9/10/2024
    Update weapon

---
+++ Assets/Script/Bullet.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void Start()
+    {
+        Destroy(gameObject, 2f);
+    }
+    private void FixedUpdate()
+    {
+        rb.velocity = transform.up * 4;
+    }
+}
--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] public SpriteRenderer avatar;
+
     private float moveSpeed = 3f;
     private Rigidbody2D rb;
     private Vector2 v;
@@ -13,20 +15,15 @@ public class PlayerMovement : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
     }
 
-    private void Movement()
+    private void Update()
     {
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
-        v = new Vector2 (x, y).normalized;
-    }
-
-    private void Update()
-    {
-        Movement();
+        v = new Vector2(x, y).normalized;
     }
 
     private void FixedUpdate()
     {
-        rb.velocity = new Vector2(v.x * moveSpeed * Time.deltaTime, v.y * moveSpeed * Time.deltaTime);
+        rb.velocity = new Vector2(v.x * moveSpeed, v.y * moveSpeed);
     }
 }
