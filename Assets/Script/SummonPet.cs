1efbef4348c11d16e492fa30e3cf7123184d846b
NguyenBach1401 <nguyenbachnhi1401@gmail.com>
2024-10-31 21:42:52 +0700

    them pet ki nang va ban tu dong

---
+++ Assets/Script/SummonPet.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using Unity.Netcode;
+
+public class SummonPet : NetworkBehaviour
+{
+    [SerializeField] private GameObject Pet;
+    [SerializeField] private float summonDistance = 2.0f;
+
+    void Update()
+    {
+        if (IsOwner && Input.GetKeyDown(KeyCode.E))
+        {
+           
+            SummonPetServerRpc();
+        }
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void SummonPetServerRpc()
+    {
+        if (IsServer)
+        {
+            Vector3 summonPosition = transform.position + new Vector3(Random.Range(-summonDistance, summonDistance), Random.Range(-summonDistance, summonDistance), 0);
+            var pet = Instantiate(Pet, summonPosition, Quaternion.identity);
+            pet.GetComponent<NetworkObject>().Spawn();
+
+            
+        }
+    }
+}
